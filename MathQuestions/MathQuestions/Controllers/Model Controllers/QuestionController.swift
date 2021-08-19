39e197795adce35716d2dbd587eb60e0fc8cdae5
The-Natalie <natalie.diana.hall@gmail.com>
2021-08-18 20:49:46 -0400

    added math questions to QuestionController

---
+++ MathQuestions/MathQuestions/Controllers/Model Controllers/QuestionController.swift
@@ -0,0 +1,31 @@
+//
+//  QuestionController.swift
+//  MathQuestions
+//
+//  Created by Natalie Hall on 8/18/21.
+//
+
+import Foundation
+
+class QuestionController {
+    
+    static let questions = [
+        Question(text: "11 + 33 = ?", answers: ["43", "34", "55"], correctAnswers: "44"),
+        Question(text: "9 x 7 = ?", answers: ["66", "54", "72"], correctAnswers: "63"),
+        Question(text: "18 + 18 = ?", answers: ["46", "38", "40"], correctAnswers: "36"),
+        Question(text: "7 x 7 = ?", answers: ["51", "42", "52"], correctAnswers: "49"),
+        Question(text: "72 - 44 = ?", answers: ["116", "112", "22"], correctAnswers: "28"),
+        Question(text: "6 x 4 = ?", answers: ["26", "18", "28"], correctAnswers: "24"),
+        Question(text: "55 + 17 = ?", answers: ["69", "78", "73"], correctAnswers: "72"),
+        Question(text: "12 x 5 = ?", answers: ["55", "65", "52", "70"], correctAnswers: "60"),
+        Question(text: "23 - 35 = ?", answers: ["-18", "-14", "-11"], correctAnswers: "-12"),
+        Question(text: "10 x 12 = ?", answers: ["102", "122", "140"], correctAnswers: "120"),
+        Question(text: "27 - 14 = ?", answers: ["11", "7", "15" ], correctAnswers: "13"),
+        Question(text: "3 x 15 = ?", answers: ["35", "30", "40"], correctAnswers: "45"),
+        Question(text: "66 + 66 = ?", answers: ["112", "126", "118"], correctAnswers: "132"),
+        Question(text: "8 x 8 = ?", answers: ["58", "60", "56"], correctAnswers: "64"),
+        Question(text: "14 + 16 = ?", answers: ["28", "42", "38"], correctAnswers: "30")
+    ]
+    
+    
+}  // End of Class
